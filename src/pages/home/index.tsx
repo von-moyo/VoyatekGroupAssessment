7f366594c853ef401e7fbb389dd13a04f8780f7c
Florish Adekogbe <florishadekogbe@gmail.com>
2025-07-25 18:08:12 +0100

    fixed build errors

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { TravelHero, TravelPlannerApp, TripItineraries } from '../../components';
+import { TravelHero, TripItineraries } from '../../components';
 
 export const Home: React.FC = () => {
   return (
     <div className="min-h- bg-white p-6 space-y-20">
       <TravelHero/>
       <TripItineraries/>
-      {/* <TravelPlannerApp/> */}
     </div>
   );
 };
